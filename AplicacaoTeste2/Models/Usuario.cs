beee86ab7c6a40cf9ca25077991027056170a65a
Ajala <Ajala@DESKTOP-AIP4D1G>
2019-10-03 20:37:38 -0400

    Modificaçãoes para teste

--- AplicacaoTeste2/Models/Usuario.cs
+++ AplicacaoTeste2/Models/Usuario.cs
@@ -12,7 +12,7 @@ namespace ApiAgendaDocumentos.Models
         public int usuario_id { get; set; }
         public string nome { get; set; }
         public string login { get; set; }
-        public string senha { get; set; }
+        public string senha { get; set; }   
         public string email { get; set; }
         public byte[] avatar { get; set; }
     }
